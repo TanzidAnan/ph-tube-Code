7530410896af6d5642eb5570303ee96d637681cd
TanzidAnan <tanzid.anan125@gmail.com>
2024-11-12 20:21:50 +0600

    firebase confige

--- src/Utils/Routs.jsx
+++ src/Utils/Routs.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayouts from "../Compontents/MainLayouts/MainLayouts";
 import Home from "../Compontents/Home/Home";
+import About from "../Compontents/About/About";
 
 const routes = createBrowserRouter([
     {
@@ -10,6 +11,10 @@ const routes = createBrowserRouter([
             {
                 path:'/',
                 element:<Home></Home>
+            },
+            {
+                path:'/about',
+                element:<About></About>
             }
         ]
     }
