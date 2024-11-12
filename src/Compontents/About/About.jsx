0adfd0cc8687d96b2e9c4827e71be19f097ca3f2
TanzidAnan <tanzid.anan125@gmail.com>
2024-11-13 00:00:39 +0600

    user

--- src/Compontents/About/About.jsx
+++ src/Compontents/About/About.jsx
@@ -2,13 +2,16 @@ import { useContext } from "react";
 import { authContext } from "../MainLayouts/MainLayouts";
 
 const About = () => {
-    const {hendleGoogleLogin,user,setUser} =useContext(authContext);
-    console.log(setUser)
+    const {hendleGoogleLogin,user} =useContext(authContext);
+    console.log(user)
     
     return (
         <div>
             <h1>this is About</h1>
             <button onClick={hendleGoogleLogin} className="btn ">Google Login</button>
+            {
+                user && <p>{user?.email}</p>
+            }
         </div>
     );
 };
