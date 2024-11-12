b060b712a957f15b423e60e7c96ef5befe6e0233
TanzidAnan <tanzid.anan125@gmail.com>
2024-11-12 23:46:02 +0600

    goole login

--- src/Compontents/About/About.jsx
+++ src/Compontents/About/About.jsx
@@ -2,11 +2,12 @@ import { useContext } from "react";
 import { authContext } from "../MainLayouts/MainLayouts";
 
 const About = () => {
-    const authCont =useContext(authContext);
-    console.log(authCont)
+    const {hendleGoogleLogin} =useContext(authContext);
+    
     return (
         <div>
             <h1>this is About</h1>
+            <button onClick={hendleGoogleLogin} className="btn ">Google Login</button>
         </div>
     );
 };
