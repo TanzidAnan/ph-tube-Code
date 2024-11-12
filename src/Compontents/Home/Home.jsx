bf5f21d4bdabb89b4eed8253c23a1235dca8ef82
TanzidAnan <tanzid.anan125@gmail.com>
2024-11-12 22:24:53 +0600

    login page google

--- src/Compontents/Home/Home.jsx
+++ src/Compontents/Home/Home.jsx
@@ -1,9 +1,26 @@
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import React from 'react';
+import auth from '../../firebase.confige';
+
 
 const Home = () => {
+
+    const googleProdider =new GoogleAuthProvider()
+
+    const hendleGoogleLogin =() =>{
+        signInWithPopup(auth,googleProdider)
+        .then(result =>{
+            console.log(result.user)
+        })
+        .catch(error =>{
+            console.log(error.message)
+        })
+
+    }
     return (
         <div>
             <h1>This is Home</h1>
+            <button onClick={hendleGoogleLogin} className='btn bg-red-400'>Google Add</button>
         </div>
     );
 };
