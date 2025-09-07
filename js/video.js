9301b636686b18103566e4f6e2de54ebb86e48e2
TanzidAnan <tanzid.anan125@gmail.com>
2025-09-07 19:53:13 +0600

    api data show disply data

--- js/video.js
+++ js/video.js
@@ -13,12 +13,20 @@
 const loadCategoride = () => {
 fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
 .then(res =>res.json())
-.then(data =>console.log(data))
+.then(data =>displayCategories(data.categories))
 .catch((error) =>console.log(error))
 }
 
-const displayCategories = () => {
-
+const displayCategories = (categories) => {
+    const categoriesContainer =document.getElementById('categories')
+    categories.forEach((item)=> {
+        console.log(item)
+        const button =document.createElement('button');
+        button.classList ='btn bg-red-400 text-white font-bold text-lg hover:bg-blue-300';
+        button.innerText =item.category;
+        categoriesContainer.append(button)
+    })
+    
 }
 
 
